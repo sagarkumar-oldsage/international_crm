5514b4899f8d72d44166fdda936b5e7c7efb13c9
sagarkumar-oldsage <sagar.kumar@poornima.edu.in>
2026-05-22 12:59:58 +0530

    feat: serve backend api via vercel /api/v1 function

---
+++ api/v1/[...route].ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import serverless from "serverless-http";
+import { createApp } from "../../../apps/api/src/create-app";
+
+type NodeHandler = (req: unknown, res: unknown) => Promise<unknown>;
+
+let cachedHandler: NodeHandler | null = null;
+
+async function getHandler(): Promise<NodeHandler> {
+  if (cachedHandler) {
+    return cachedHandler;
+  }
+
+  const app = await createApp();
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  cachedHandler = serverless(expressApp) as NodeHandler;
+
+  return cachedHandler;
+}
+
+export default async function handler(req: unknown, res: unknown) {
+  const appHandler = await getHandler();
+  return appHandler(req, res);
+}
